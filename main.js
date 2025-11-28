7390ecdc4d106d1d4bc6a59447cb0baf6e5f8e4f
Andrzej STANDOFF <andrzejlemieszewski1ti@gmail.com>
2025-11-28 10:52:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
